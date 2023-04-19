d9421df0ca27ff5004313cde6ad510f652c624a4
Freddy Tacuri <freddyltacuri@gmail.com>
2023-04-19 01:12:44 -0500

    Haciendo el logo de inicio

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+const logo = document.querySelector('.svgLogo');
 
 anime ({
-    targets: '.gLogo',
-    backgroundColor: '#FFF',
-    duration: 5000
+    targets: '.svgLogo',
+    scale: [0.9, 1.05],
+    opacity: [1, 0],
+    update: function(anim){
+        logo.style.filter = 'blur(' + 5 * anim.progress / 50 + 'px)'
+        logo.style.filter = 'drop-shadow(0 0 1em #d1a392)'
+    },
+    easing: 'easeInExpo',
+    duration: 11000,
+    direction: 'reverse',
+    loop: true
 })
