d907d275efcb550d0b9a1f1025e8f0f0b9111b8c
Freddy Tacuri <freddyltacuri@gmail.com>
2023-04-19 20:57:52 -0500

    Implementando interaccion del teclado con el logo de inicio

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const logo = document.querySelector('.svgLogo');
+addEventListener("keydown", function(e) {
+    e.preventDefault();
+    if ( e.code == 'Enter') {
+        logo.style.visibility = 'hidden';
+    }if ( e.code == 'Escape') {
+        logo.style.visibility = 'visible';
+    }
+});
 
 anime ({
     targets: '.svgLogo',
     scale: [0.9, 1.05],
     opacity: [1, 0],
     update: function(anim){
-        logo.style.filter = 'blur(' + 5 * anim.progress / 50 + 'px)'
-        logo.style.filter = 'drop-shadow(0 0 1em #d1a392)'
+        logo.style.filter = 'drop-shadow(0 0 '+ anim.progress / 10 +'px #d1a392) blur(' + anim.progress / 50 + 'px)';
+        logo.style.strokeWidth = (anim.progress / 50) + 0.7 + 'em';
     },
     easing: 'easeInExpo',
     duration: 11000,
     direction: 'reverse',
-    loop: true
-})
+});
