aabfa63d6ecd7fd0a295046c898f44c6aa4bda0d
Freddy Tacuri <freddyltacuri@gmail.com>
2023-05-09 13:47:08 -0500

    Correccion de errores

--- script.js
+++ script.js
@@ -49,42 +49,65 @@ anime ({
 });
 
 if(screen.width <= 900){
+    window.alert("ADVERTENCIA -> Esta pagina aun no esta optimizada para celulares o pantallas pequeñas. :( Por lo que es posible que no funcione o se visualice mal.")
     document.getElementById("mensaje").innerHTML = "Toca la pantalla";
-    caption.style.backgroundColor = "aqua";
 };
 
 window.scrollTo( 0, 0 );
 
-//? Animación bonfire pendiente
+//?Opacidad scroll
+const main = document.querySelector('.main');
+const img3 = document.querySelector('.img3');
+const div = document.querySelector('.div');
+const divImg = document.querySelector('.div-img');
+const body = document.querySelector('body');
+var executed = false;
+
+body.addEventListener('scroll', function(event) {
+    var height = (body.scrollTop/(body.scrollHeight-window.innerHeight));//0 -> 1
+    
+    function middleOpacity (element) {
+        if (height > 0.5){
+            element.style.opacity = (1-height)*2 + '0';
+        }else{
+            element.style.opacity = (height*2) -0.01 + '0';
+        }
+    };
+    
+    main.style.opacity = 0.9 - (height*0.9) + '0';
+    div.style.boxShadow = '0 0 50 ' + 1 - (height*3) + 'px #000000;';
+    middleOpacity(div);
+    img3.style.opacity = height + '0';
+});
+
+//? Animación bonfire
 var fragment = document.createDocumentFragment();
 var numberOfFires = 100;
 var duration = 5000;
 var midScreenX = window.innerWidth / 2;
 var midScreenY = window.innerHeight / 2;
 
-function createFire() {
+function createFire(top, left) {
     var fire = document.createElement('div');
-    radius = (Math.random() * (100 - 10 + 1) + 10);
     fire.classList.add('fire');
     fire.style.backgroundColor = '#f0eaf2';
     fire.style.boxShadow = '0 5px 5px #ec9633';
     fire.style.borderRadius = '10px';
-    fire.style.width = '5px';
-    fire.style.height = '10px';
+    fire.style.width = '3px';
+    fire.style.height = '5px';
+    fire.style.top = top;
+    fire.style.left = left;
     return fire;
 }
-
 for (var i = 0; i < numberOfFires; i++){
-    var fires = createFire();
-    var angle = Math.random() * Math.PI * 2;
-    console.log(angle)
+    var fires = createFire('39vw', '50%');
     function animation(target) {
         anime ({
             targets: target,
-            scale: [0.5, 1, 0],
-            opacity: [0, 1.5],
-            left: [anime.random((midScreenX-35),(midScreenX+22)) + 'px', anime.random(400, 1200) + 'px'],
-            top: [640 + 'vw', -1000 + 'px'],
+            scale: [0, 1],
+            opacity: [0, 1, 0],
+            translateX: [anime.random(-20, 20), anime.random(-100, 100)],//?[punto X inicial, punto X final]
+            translateY: [anime.random(10, 15), anime.random(-350, 0)],//?[punto Y inicial, punto Y final]
             delay: (duration/numberOfFires) * i,
             duration: duration,
             easing: 'linear',
@@ -98,32 +121,6 @@ document.body.appendChild(fragment);
 
 
 
-
-const main = document.querySelector('.main');
-const img3 = document.querySelector('.img3');
-const div = document.querySelector('.div');
-const divImg = document.querySelector('.div-img');
-const body = document.querySelector('body');
-var executed = false;
-
-body.addEventListener('scroll', function(event) {
-    var height = (body.scrollTop/(body.scrollHeight-window.innerHeight));//0 -> 1
-    
-    function middleOpacity (element) {
-        if (height > 0.5){
-            //console.log("+-");
-            element.style.opacity = (1-height)*2 + '0';
-        }else{
-            //console.log("-+");
-            element.style.opacity = (height*2) -0.01 + '0';
-        }
-    };
-    
-    main.style.opacity = 0.9 - (height*0.9) + '0';
-    div.style.boxShadow = '0 0 50 ' + 1 - (height*3) + 'px #000000;';
-    middleOpacity(div);
-    img3.style.opacity = height + '0';
-});
 
 
 
